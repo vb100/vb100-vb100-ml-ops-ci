0f59df5b74bc1067d80a8bb662b5c09deb9c3401
Vytautas Bielinskas <vytautas.bielinskas@gmail.com>
2021-03-23 09:49:04 +0200

    Update model.py

--- model.py
+++ model.py
@@ -17,13 +17,13 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
   # Plot the predictions in red (predictions were made on the test data)
   plt.scatter(test_data, predictions, c="r", label="Predictions")
   # Show the legend
-  plt.legend(shadow='True')
+  plt.legend(shadow='True', fontsize=7)
   # Set grids
   plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
   # Some text
-  plt.title('Model Results', family='Arial', fontsize=12)
-  plt.xlabel('X axis values', family='Arial', fontsize=9)
-  plt.ylabel('Y axis values', family='Arial', fontsize=9)
+  plt.title('Model Results', fontsize=12)
+  plt.xlabel('X axis values', fontsize=9)
+  plt.ylabel('Y axis values', fontsize=9)
   # Show
   plt.savefig('model_results.png', dpi=120)
 
