65c474e4bbdd30bc3c008bced3b77f5be41e73b7
Vytautas Bielinskas <vytautas.bielinskas@gmail.com>
2021-03-23 09:41:23 +0200

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
   """
   Plots training data, test data and compares predictions.
   """
-  plt.figure(figsize=(6, 4.25))
+  plt.figure(figsize=(5.5, 3.65))
   # Plot training data in blue
   plt.scatter(train_data, train_labels, c="b", label="Training data")
   # Plot test data in green
@@ -21,9 +21,9 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
   # Set grids
   plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
   # Some text
-  plt.title('Model Results', family='Arial', fontsize=14)
-  plt.xlabel('X axis values', family='Arial', fontsize=11)
-  plt.ylabel('Y axis values', family='Arial', fontsize=11)
+  plt.title('Model Results', family='Arial', fontsize=12)
+  plt.xlabel('X axis values', family='Arial', fontsize=9)
+  plt.ylabel('Y axis values', family='Arial', fontsize=9)
   # Show
   plt.savefig('model_results.png', dpi=120)
 
